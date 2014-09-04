8bf14b6b9f6c69c54a7aa0b13eb85787c2e71df6
Krzysztof Bijakowski <k.bijakowski@wp.pl>
2014-09-04 20:32:02 +0200

    Population generation fixed

--- payments.m
+++ payments.m
@@ -96,19 +96,26 @@ end
 % The most basic implementation of it is just to iterate through the users
 % charging them with some random part of their assets until the required
 % payment is not collected.
-initialPopulation = ones(options.populationSize);
+
+initialPopulation(options.populationSize).solution = {};
+
 for i = 1 : options.populationSize
+    currentSolution = {};
+    currentSolution.meeting = meetings;
+    currentSolution.fittness = 0.0;
+
     for j = 1 : meetingCount
         currentMeeting = meetings(j);
+        currentSolution.meeting(j).charges = zeros (memberCount, meansCount);
         currentAssets = currentMeeting.assets;
         totalMembers = size(currentMeeting.assets, 1);
         collectedMoney = 0;
         while collectedMoney ~= currentMeeting.requiredMoney
             % Pick up a random person
-            memberIndex = mod(random(), totalMembers) + 1;
+            memberIndex = randi([1 totalMembers],1,1);
             % Pick random payment method from accessible ones
-            meansIndex = mod(random(), length(currentMeeting.means)) + 1;
-            
+            meansIndex = currentMeeting.means(randi([1 length(currentMeeting.means)],1,1));
+
             % Find out what the contribution should be
             if currentAssets(memberIndex, meansIndex) >= (currentMeeting.requiredMoney - collectedMoney)
                 % He has enough money to finish up the payment by himself
@@ -117,7 +124,7 @@ for i = 1 : options.populationSize
             else
                 % He don't have enough money to finish up the payment by
                 % himself so take a random part of what he's got
-                chargeRatio = (mod(random(), 100) + 1) / 100;
+                chargeRatio = rand();
                 % Get the contribution
                 contribution = chargeRatio * currentAssets(memberIndex, meansIndex);
             end
@@ -125,14 +132,16 @@ for i = 1 : options.populationSize
             currentSolution.meeting(j).charges(memberIndex, meansIndex) = currentSolution.meeting(j).charges(memberIndex, meansIndex) + contribution;
             currentAssets(memberIndex, meansIndex) = currentAssets(memberIndex, meansIndex) - contribution;
             collectedMoney = collectedMoney + contribution;            
-        end        
+        end
     end
-    
+
     % Determine fitness
     currentSolution.fitness = calculateFitness(currentSolution);
-    population = selectFittest(population);
-    initialPopulation(i) = currentSolution;
+
+    % Add solution to array
+    initialPopulation(i).solution = currentSolution;
 end
+
 
 %% Run genetic algorithm
 % The operations are as follows:
